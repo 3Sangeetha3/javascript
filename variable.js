e04073a13173bafeba2d6481d8ca66e3794c9076
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-08 14:00:19 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,20 @@
+// JavaScript variables with var,let, orconst.
+var x = 2;
+var y = 3;
+console.log(x+y)
+x=10;
+console.log(x+y)
+
+//If you think the value of the variable can change, use let.
+let a = 5
+let b = 6;
+console.log(a+b)
+a = 10;
+console.log(a+b)
+
+//constant values and cannot be changed.
+const p = 4;
+const q = 5;
+console.log(p+q)
+///p=5; //we cannot change the value of const 
+console.log(p+q)
