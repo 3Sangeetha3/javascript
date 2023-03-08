2a285a0fe700128a33158df8f63ff584dc33b2e4
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-08 14:41:26 +0530

    dollar sign as identifier

---
+++ dollaesign.js
@@ -0,0 +1,8 @@
+//Since JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names:
+
+let $ = "Hello World";
+let $$ = 2;
+let $myMoney = 5;
+console.log($)
+console.log($$)
+console.log($myMoney)
