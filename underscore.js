d9c92b4a236d7a8d7f9ae1868fcc1b985d1324f9
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-08 14:40:03 +0530

    underscore identifier

---
+++ underscore.js
@@ -0,0 +1,8 @@
+//JavaScript treats underscore as a letter, identifiers containing _ are valid variable names:
+
+let _firstname = "sangeeta";
+let _lastname = "jadamal";
+let _age = 17;
+console.log(_firstname)
+console.log(_lastname)
+console.log(_age)
