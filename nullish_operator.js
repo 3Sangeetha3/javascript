222f31beb69dd79714a685d5a2dbd1247075fc23
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-22 20:22:50 +0530

    nullish_operator

---
+++ nullish_operator.js
@@ -0,0 +1,9 @@
+//The ?? operator returns the first argument if it is not nullish (null or undefined).
+//Otherwise it returns the second argument.
+let name = null;
+let text = "second line";
+let result = name ?? text;
+console.log(result)
+let firstname = "sangeetha";
+let marks = null;
+console.log(firstname??marks)
