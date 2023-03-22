8502b89d10fd2e43c18b97783b4f2ca56261240e
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-22 20:03:58 +0530

    arthematic operator

--- arthematicoperator.js
+++ arthematicoperator.js
@@ -1,8 +1,12 @@
-let x = 10;
-let y = 20;
+var x = 10;
+var y = 20;
 console.log(x+y)
 console.log(x-y)
 console.log(x*y)
 console.log(x/y)
 console.log(x%y)
 console.log(x**y)
+console.log(x++)//postincrement ; first assign then increases or decreases  then the value
+console.log(x)
+console.log(++y)//preincrement ; first increases or decreases  then the value then assign the value
+console.log(y)
