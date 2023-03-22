aba4b1e4d248653305d5d7c95897c9217a308490
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-03-22 20:10:04 +0530

    assignment operator

---
+++ asssignment_operator.js
@@ -0,0 +1,9 @@
+// assignment operator assign values to JavaScript variables.
+
+let x = 2, y = 3;
+console.log(x+=y)
+console.log(x-=y)
+console.log(x*=y)
+console.log(x/=y)
+console.log(x**=y)
+console.log(x%=y)
